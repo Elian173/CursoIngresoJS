ed89e0e7403ee3a1dec0e931896f15f8fdd478ce
unknown <rojaselian173@gmail.com>
2019-02-08 20:50:56 -0300

    numeroseis

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,18 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var nDos;
+    var Sumado;
+    var nUno;
+
+    nUno=document.getElementById("numeroUno").value;
+
+    nDos=document.getElementById("numeroDos").value;
+
+    Sumado=parseInt(nUno)+parseInt(nDos);
+    
+    alert (Sumado);
+    
 
 }
 
