419cebe696a477346fe88cb9342edd129fc768e1
unknown <rojaselian173@gmail.com>
2019-02-08 20:54:18 -0300

    siete

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,61 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var nDos;
+    var Sumado;
+    var nUno;
+
+    nUno=document.getElementById("numeroUno").value;
+
+    nDos=document.getElementById("numeroDos").value;
+
+    Sumado=parseInt(nUno)+parseInt(nDos);
+    
+    alert (Sumado);
 }
 
 function restar()
 {
-	
+	var nDos;
+    var Sumado;
+    var nUno;
+
+    nUno=document.getElementById("numeroUno").value;
+
+    nDos=document.getElementById("numeroDos").value;
+
+    Sumado=parseInt(nUno)-parseInt(nDos);
+    
+    alert (Sumado);
 }
 
 function multiplicar()
 { 
-	
+	var nDos;
+    var Sumado;
+    var nUno;
+
+    nUno=document.getElementById("numeroUno").value;
+
+    nDos=document.getElementById("numeroDos").value;
+
+    Sumado=parseInt(nUno)*parseInt(nDos);
+    
+    alert (Sumado);
 }
 
 function dividir()
 {
-	
+	var nDos;
+    var Sumado;
+    var nUno;
+
+    nUno=document.getElementById("numeroUno").value;
+
+    nDos=document.getElementById("numeroDos").value;
+
+    Sumado=parseInt(nUno)/parseInt(nDos);
+    
+    alert (Sumado);
 }
 
