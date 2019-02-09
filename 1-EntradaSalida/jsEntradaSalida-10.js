97c61dfa1f8bf24ea4792649b6e51d8180e860c5
unknown <rojaselian173@gmail.com>
2019-02-08 22:06:50 -0300

    wiiii

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+    
+    var sueldoBr;
+    var descuento;
+    var total;
+       
+        sueldoBr= document.getElementById("importe").value;
+    
+        descuento= (parseInt(sueldoBr))*0.25;
+    
+        total=parseInt (sueldoBr)-(descuento);
+    
+        document.getElementById("resultado").value=(total);
+        
 	
 }
