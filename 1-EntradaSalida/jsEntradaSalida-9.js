bd01f6f5a0304fc5e20aff86f22dbec34517333b
unknown <rojaselian173@gmail.com>
2019-02-08 22:01:19 -0300

    siiiiiii

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+var sueldoBr;
+var aumento;
+var total;
+   
+    sueldoBr= document.getElementById("sueldo").value;
 
-	
+    aumento= (parseInt(sueldoBr))*0.10;
+
+    total=parseInt (sueldoBr)+(aumento);
+
+    document.getElementById("resultado").value=(total);
+    
+
+    
+
 }
