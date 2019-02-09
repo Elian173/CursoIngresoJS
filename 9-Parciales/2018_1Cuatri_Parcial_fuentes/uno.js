b9c919c844b9f2169deb9229d532affa8e0842ab
unknown <rojaselian173@gmail.com>
2019-02-08 22:14:45 -0300

    rectaaangulo

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
+
+
+    ancho=prompt ("ingrese Ancho");
+
+    largo=prompt ("ingrese largo");
+
+
+    perimetro=(parseInt (ancho)+parseInt (largo));
+
+    alert ("EL perimetro de este rectangulo es "+(perimetro*2)+" cm");
+
+
+
 
 }
