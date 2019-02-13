05b2e37a2da6ceaa5828a2427f8b7a874d0cf709
Elian173 <rojaselian173@gmail.com>
2019-02-13 20:36:56 -0300

    tpdos

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,64 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var ancho;
+    var largo;
+    var radio;
+    var perimetro;
 
+    ancho=document.getElementById("Ancho").value;
+    largo=document.getElementById("Largo").value;
+
+    parseInt (ancho)
+    parseInt (largo)
+
+    perimetro=(ancho)*2+(largo)*2;
+
+    alambres1=perimetro/3;
+
+    alert ("EL perimetro de este terreno es "+(perimetro)+" metros , y se necesitan 3 alambres de "+alambres1+" metros cada 1");
 }
 function Circulo () 
 {
-	
+    var diametro
+    var alambres2
+    var perimetroCirc
+
+    radio=document.getElementById("Radio").value;
+
+    parseInt(radio);
+
+    diametro=radio*2;
+
+    perimetroCirc=(diametro*3.14);
+
+    alambres2= perimetroCirc/3;
+
+    alert ("Se necesitan 3 alambres de "+alambres2+" metros c/u");
+    
+    
 }
 function Materiales () 
 {
+    var superficie
+    var cemento
+    var cal
+
+    ancho=document.getElementById("Ancho").value;
+    largo=document.getElementById("Largo").value;
+
+    parseInt (ancho);
+    parseInt (largo);
+
+    superficie=(ancho)*(largo);
+    
+    parseInt (superficie)
+
+    cemento=superficie/2;
+    cal=superficie/3;
+
+    alert ("Se necesitan "+cemento+" bolsas de cemento y "+cal+" bolsas de cal , para cubrir la superficie de "+superficie+" metros cuadrados");
+
+
 	
 }
