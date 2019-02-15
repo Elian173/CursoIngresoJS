617989163b394646f4798f9d1e04ad257ef65f12
elian173 <rojaselian173@gmail.com>
2019-02-15 20:22:24 -0300

    recu3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+var precio
+var amigos
+var propina
+var pagaCadaUno
+var total
+var iva
+
+parseInt (precio);
+parseInt (amigos);
+parseInt (propina);
+parseInt (pagaCadaUno);
+parseInt (total);
+parseInt(iva);
+
+precio=1000;
+amigos=5;
+
+
+propina=precio*(0.1);
+total = precio + propina;
+iva=precio*(0.21)
+
+pagaCadaUno= (total/amigos);
+
+
+alert ("El precio es "+precio+" la propina es $"+propina+" en total seria $"+
+total +"(sin contar el iva de $"+iva+") , y cada uno de los amigos deveria pagar $"+
+pagaCadaUno);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,22 @@
 
 function mostrar()
 {
+var mascotaUno
+var mascotaDos
+var suma
+
+mascotaUno=10;
+mascotaDos=20;
+
+
+
+parseInt (mascotaUno);
+parseInt (mascotaDos);
+
+suma=mascotaUno+mascotaDos;
+
+alert("Tenes dos mascotas , xxxx y xxxxx que pesan "+mascotaUno+" y "+mascotaDos+
+" kilos , la suma de los kilos es "+ suma );
+
 
 }
