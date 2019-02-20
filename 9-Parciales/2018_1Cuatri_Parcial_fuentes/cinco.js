7d5aa5ceb2385ada4f7eca8e7d3287c038cb0ab4
alumno <alumno>
2019-02-20 19:52:38 -0300

    parcialswitc

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+var planeta;
+
+planeta= prompt ("Ingrese el nombre de un planeta del sistema solar");
+
+switch (planeta){
+    case "tierra": alert("aca vivimos");
+    break;
+    case "mercurio":
+    case "venus" : alert ("acá hace más calor")
+    break;
+    case "marte":
+    case "jupiter":
+    case "urano":
+    case "neptuno" :
+    case "saturno" : alert("aca hace mas frio")
+    break;
+    default :alert("el planeta no existe")
+}
 
 }
+
