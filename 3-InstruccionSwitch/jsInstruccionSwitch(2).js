82b34cc8acd01da6e6ed625a91ec874bba5b9ab7
alumno <alumno>
2019-02-20 19:42:08 -0300

    -s

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,7 +5,20 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
-
+switch (mesDelAño){
+    case "Agosto":
+    case "Julio": alert("Abrigate que hace frio.");
+    break;
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+    case "Junio": alert ( "Falta para el invierno.");
+    break;
+    default : alert("Ya pasamos el frio, ahora calor!!!.");
+    break;
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -4,8 +4,18 @@ function mostrar()
 var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
-	
-	
+    
+    parseInt(laHora)
+
+	switch (parseInt(laHora)) {
+        case 7:
+        case 8 :
+        case 9:
+        case 10:
+        case 11 : alert ("Es de mañana");
+        break;
+       
+    }
 
 
 
