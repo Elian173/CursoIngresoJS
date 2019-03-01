a960a867d25323af776ffcd5247a78527ae6d6cc
alumno <alumno>
2019-03-01 18:54:23 -0300

    a

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
 
+    var numero
+    var letra
+    var cantPares=0
+    var cantImpares=0
+    var cantCeros=0
+    var SumaPositivos=0
+    var SumaNegativos=0
+    var letraMax
+    var numMax
+    var numMin
+    var letraMin
+    var respuesta=true
+    var bandera
+while ( respuesta ) {
+
+    letra = prompt("Ingrese una letra") ; 
+    parseInt(letra);
+                                        while (letra=!isNaN(letra)) {
+                                            letra = prompt("Ingrese una letra!!!");
+                                            parseInt(letra);
+                                        };
+
+/*---------------------------------------*/
+    numero=prompt("ingrese un numero menor que 100 y mayor que -100");
+    numero=parseInt(numero);            
+                                        while(isnan(numero)||(numero>100||numero<-100)){
+                                        numero = prompt("ingrese un numero menor que 100 y mayor que -100!!!");
+                                        parseInt(numero);
+                                    };
+/*---------------------------------------*/
+if (bandera){
+    bandera=false;
+    numMax=numero ; numMin = numero ;
 }
+
+    respuesta =confirm("ingresar otra combinacion?");
+    
+    
+}
+
+
+  }
