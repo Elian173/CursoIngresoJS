7b010f1004674d1c255afde6f038689ea3a9fe02
alumno <alumno>
2019-03-01 19:30:26 -0300

    ejercicio7recu
    Signed-off-by: alumno <alumno>

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,76 @@
 function mostrar()
 {
+    var velocidad
+    var combustible
+    var contadorLiquido= 0
+    var contador = 0
+    var velocidadMasBaja = 250
+    var sumaDeVelocidades = 0
+    var combustibleVelBaja = 0
+    var velocidadMasAlta=0
 
-}
+while (contador<5) {
+
+    contador ++ ; 
+///////////
+    velocidad = prompt ("Ingrese la velocidad del vehiculo (entre 0 y 250)") ;
+    velocidad=parseInt(velocidad)  ;
+                    while(isNaN(velocidad)||velocidad>250||velocidad<0) {
+                                        velocidad=prompt("error , ingrese la velocidad del vehiculo")
+                                        velocidad=parseInt(velocidad)
+                                       };
+////////////
+    combustible= prompt(" Ingrese el tipo de combustible (l) para Liquido o (s) para Solido");
+
+                    while(combustible!="l"&&combustible!="s"){
+                        combustible= prompt(" error , ingrese el tipo de combustible (s) o (l)")
+                                               };
+////////////
+
+if (velocidad>velocidadMasAlta&&combustible=="s")
+{
+     velocidadMasAlta=velocidad
+};
+
+////////////
+
+if (velocidad>100&&combustible=="l")    {contadorLiquido++};
+
+////////////
+
+if (combustible=="s"&&velocidad<=velocidadMasBaja) 
+        { 
+        velocidadMasBaja=velocidad ;
+        combustibleVelBaja="Solido"
+        } 
+        else {
+            if (combustible=="l"&&velocidad<=velocidadMasBaja)
+             {
+                velocidadMasBaja=velocidad ;
+                combustibleVelBaja="Liquido"
+            }
+        }
+         
+
+////////////
+
+sumaDeVelocidades+=velocidad;
+
+////////////
+
+};
+
+
+
+alert ( "El promedio de las velocidades totales es de "+sumaDeVelocidades/5+" km/h. La velocidad mas baja fue "+velocidadMasBaja+
+" km/h . El combustible del vehiculo con la velocidad mas baja era "+combustibleVelBaja+" y la cantidad de vehiculos con "+
+"combustible liquido y velocidad superior a 100 km/h es de "+contadorLiquido+" vehiculo/s.");
+
+
+};
+
+
+
+
+
+
