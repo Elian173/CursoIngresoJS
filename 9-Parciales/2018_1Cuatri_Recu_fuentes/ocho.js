3137d08c1c161eb40cc82107b05c663832afef39
elian173 <rojaselian173@gmail.com>
2019-03-02 23:40:35 -0300

    ejDosBien(en el ocho)

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,91 +1,138 @@
 function mostrar()
 {
-    var territorio
-    var pais
-    var contador=0
-    var sumaDeTerritorios = 0
-    var bandera=true
-    var pregunta = true
-    var contadorCien=0
-    var contadorMasCien=0
-    var contadorMenosCien=0
-    var contadorImpar=0
-    var territorioMasChico
-    var paisMasChico
-    var primeroConCien
 
-    while (pregunta)
+    /* realizar un algoritmo que permita iterar el uso de datos , un pais , y
+    la superficie de su territorio (validar) hasta que el usuario quiera e informar
+    el ingreso por document.write.
+    averiguar  = la cantidad de paises con superficie impar , 
+    la cantidad de paises con superficie menor , mayor o igual a 100 
+    - el nombre del primer pais cuya superficie fue mayor a 100
+    -el promedio de kilometros ingresados 
+    -el nombre del que menos territorio tiene*/
 
- {
-contador++;
-//Pido territorio//
-territorio = prompt ("Ingrese la superficie del territorio") ;
 
-territorio=parseInt(territorio) ;
+    var pais ; var superficie ; var paisConMenorSuperficie ; var superficieMasChica; 
+    
+    var primeroConCienBandera=true ;var nombrePrimeroConCien; var sumaDeSuperficies=0
+    
+    var contadorImpar=0 ; var contadorMenoresACien=0 ;  i = 0 ;
 
-    while(isNaN(territorio)||territorio<0)
+    var  contadorMayoresACien=0 ;var contadorIgualACien=0 ; var respuesta =true 
+
+
+while (respuesta)
+{
+    i++ ;
+/*Pregunto el pais*/
+
+pais = prompt("Ingrese el nombre del pais") ;
+
+    while ( !isNaN (pais) )
     {
-        territorio=prompt("error , ingrese superficie de territorio");
-        territorio=parseInt(territorio);
+        pais = prompt("Error.Porfavor ingrese el nombre del pais");
     };
 
-//pido pais//
+/* Pregunto la superficie*/
 
-pais= prompt(" Ingrese un pais");
+superficie = prompt("Ingrese la superficie de ese pais") ;
 
-    while (pais=!isNaN(pais))
-{
-pais = prompt(" error , ingrese el pais")
-}
+superficie = parseInt(superficie) ;
 
-//territorios mayores iguales y menores a 100 y 1ro con 100//
-if (bandera&&territorio>100) 
-{
-    primeroConCien=pais
-
-}
-
-if (territorio=100) 
+    while (isNaN (superficie) || superficie < 0 )
     {
-    contadorCien++
-    } 
-    else{
-             if (territorio>100) 
-             {
-                 contadorMasCien++
-             }
-              else{
+        superficie = prompt("Error.Porfavor ingrese la superficie de ese pais");
+        parseInt (superficie);
+    };
 
-              if (territorio<100) 
-                 {                    
-                  contadorMenosCien++
-                 }
-             }      
-        }       
-//par o impar//
-if ((territorio%2)=!0)
+/*esto se ejecuta solo la primera vez*/
+/*igualo la superficie mas chica a la primer superficie ingresada y guardo el pais al que pertenece*/
+if (i == 1) 
 {
-    contadorImpar ++
-}
-//territorio mas chico//
-if (bandera) { 
-    territorioMasChico=territorio;
-    bandera=false;
-             };
-if(territorio<territorioMasChico)
+    superficieMasChica = superficie
+    paisConMenorSuperficie = pais
+};
+///
+/*esto se ejecuta siempre*/
+
+/*averiguo si la superfice es menor a la anterior y la asigno a la mas chica , y su correspondiente pais*/
+
+if (superficie < superficieMasChica)
 {
-    territorioMasChico=territorio
-}
-//sumo las superficies///
-sumaDeTerritorios+=territorio;
-////////////
+    superficieMasChica=superficie;
+    paisConMenorSuperficie=pais;
+};
+
+/*Cuento los paises mayores menores o iguales a 100*/ 
+
+if (superficie > 100) { contadorMayoresACien ++ }
+ else{
+    if (superficie == 100) { contadorIgualACien ++ }
+     else
+        {
+             if(superficie < 100 ) { contadorMenoresACien ++ }
+        }
+    };
+
+    /*veo si es  impar */
+
+if (superficie % 2 != 0)
+{
+    contadorImpar ++ ;
+};
+
+/*averiguo el primero con superficie mayor a 100*/
+
+if (primeroConCienBandera && superficie > 100)
+{
+     nombrePrimeroConCien = pais;
+     primeroConCienBandera=false
+};
+
+//sumo las superficies para el  promedio  de los kilometros ingresados //
+
+sumaDeSuperficies += superficie ; 
+
+// pregunta para cerrar el while
+
+respuesta = confirm("¿Agregar otro pais?");
 
 };
 
-alert("Hay "+contadorImpar+" paises con superficie impar.\n Hay "+contadorMenosCien+" paises con superficie menor a 100km."+
-"\nHay "+contadorCien+" paises con una superficie igual a 100km.\n Hay "+contadorMasCien+" paises con una supeficie mayo a 100"+
-"\n El promedio de las superficies de los paises es " + sumaDeTerritorios/contador + " . \n El pais con menor superficie es "+paisMasChico);
+document.write("Cantidad de paises con superficie impar = "+ contadorImpar);
+document.write("Cantidad de paises con superficie menor a 100= "+ contadorMenoresACien );
+document.write("Cantidad de paises con superficie iguales a 100= "+ contadorIgualACien );
+document.write("Cantidad de paises con superficie mayor a 100= "+ contadorMayoresACien );
+document.write("Nombre del pais con menor superficie = "+paisConMenorSuperficie+" (su superficie = "+ superficieMasChica + ")");
+document.write("El primer pais ingresado con superficie mayor a 100= "+nombrePrimeroConCien);
+document.write("El promedio de todos los kilometros de los paises ingresados= "+sumaDeSuperficies/i);
+
+
 
 };
 
 
+/*     
+    //valido pais MAL porque devuelve true o false//
+    
+    pais = prompt("ingrese un pais");
+
+        while (pais=!isNaN(pais))
+    {
+    pais = prompt(" error , ingrese el pais")
+    };
+    
+
+    //valido pais y devuelve lo que escribi//
+
+
+    paisBien = prompt("Ingrese el nombre del pais") ;
+
+    while ( !isNaN (paisBien) )
+    {
+        paisBien = prompt("Error.Porfavor ingrese el nombre del pais");
+    };
+
+
+alert(pais) ;
+alert(paisBien);
+*/
